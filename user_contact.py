7bc1e50510a3bd1e209006bc00012d875c9b6174
VlakBerr <boro46141@gmail.com>
2024-09-22 12:17:10 +0300

    создали представление для контакта пользователя
    Пользователь обладает именем, номером телефона и опциональным описанием

---
+++ user_contact.py
@@ -0,0 +1,15 @@
+class UserContact:
+    def __init__(self,
+                 name:str,
+                 phone_number:str,
+                 discription:str = "" ):
+        self.name = name 
+        self.phone_number = phone_number
+        self.discription = discription
+
+    def __str__(self):
+        return f'Имя: {self.name}, Номер телефона: {self.phone_number}'
+    
+
+user= UserContact('макс', '485785730')
+print(user)
